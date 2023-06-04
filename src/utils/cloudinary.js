ae1d6e1f812c1388202c47eca9d077dee9f71b3b
Sarah-2015 <sarah.ahmed.fcis@gmail.com>
2023-06-05 02:53:50 +0300

    test2 commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,11 +5,13 @@ import cloudinary from 'cloudinary';
 
 
 
-cloudinary.config({ 
-    cloud_name: 'dxmo3ijlq', 
-    api_key: '152298584562871', 
-    api_secret: 'R7TbSyf36oz9Pe-KYTfvzNt6LpY',
-    secure:true 
-  });
+cloudinary.v2.config({
+    cloud_name:process.env.cloud_name,
+    api_key:process.env.api_key,
+    api_secret:process.env.api_secret,
+    secure:true
+    
+    
+})
 
-export default cloudinary;
+export default cloudinary.v2;
